7f4780f3300f85fb611f43d423d76cfd8b99997e
kadengrumbles73 <kaden.grumbles@gmail.com>
2026-01-15 11:03:00 -0700

    finished week 2 ponder 2

--- ponder/week2/ponder2/ponder2.js
+++ ponder/week2/ponder2/ponder2.js
@@ -1,23 +1,33 @@
+const selectElem = document.querySelector('#theme-select');
+const body = document.body;
 
-let selectElem = document.querySelector('#theme-select');
-let pageContent = document.querySelector('body');
-
-selectElem.addEventListener('change', changeTheme);
+const themes = {
+    ocean: {
+        image: "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')",
+        font: "Papyrus, fantasy"
+    },
+    forest: {
+        image: "url('https://wddbyui.github.io/wdd131/images/forest.jpg')",
+        font: "Impact, sans-serif"
+    },
+    desert: {
+        image: "url('https://wddbyui.github.io/wdd131/images/desert.jpg')",
+        font: "'Big Caslon', serif"
+    }
+};
 
 function changeTheme() {
-    let current = selectElem.value;
-    if (current === 'ocean') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
-        pageContent.style.fontFamily = "Papyrus, fantasy";
-    } else if (current === 'forest') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
-        pageContent.style.fontFamily = "Impact, sans-serif";
-    } else if (current === 'desert') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
-        pageContent.style.fontFamily = "'Big Caslon', serif";
+    const choice = selectElem.value;
+    const theme = themes[choice];
+
+    if (theme) {
+        body.style.backgroundImage = theme.image;
+        body.style.fontFamily = theme.font;
     } else {
-        // default
-        document.body.style.backgroundImage = "none";
-        pageContent.style.fontFamily = "Georgia, serif";
+
+        body.style.backgroundImage = "none";
+        body.style.fontFamily = "Georgia, serif";
     }
 }
+
+selectElem.addEventListener('change', changeTheme);
