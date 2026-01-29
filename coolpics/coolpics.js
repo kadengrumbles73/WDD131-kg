722812a5fde9a31cd8a783641c90ba9c2e7bc32d
kadengrumbles73 <kaden.grumbles@gmail.com>
2026-01-29 09:47:52 -0700

    finished coolpics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -6,32 +6,3 @@ menuButton.addEventListener("click", () => {
 });
 
 
-const gallery = document.querySelector('.gallery');
-const modal = document.querySelector('dialog');
-const modalImage = modal.querySelector('img');
-const closeButton = modal.querySelector('.close-viewer');
-
-// Event listener for opening the modal
-gallery.addEventListener('click', openModal);
-
-function openModal(e) {
-    
-// Code to show modal  - Use event parameter 'e'   
-    if (e.target.tagName === 'IMG'){
-        const clickSrc = e.target.getAttribute('src');
-        const fullSrc = clickSrc.replace('-sm', '-full');
-        modalImage.src = fullSrc;
-        modal.showModal();
-    }
-}
-// Close modal on button click
-closeButton.addEventListener('click', () => {
-    modal.close();
-});
-
-// Close modal if clicking outside the image
-modal.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        modal.close();
-    }
-});
