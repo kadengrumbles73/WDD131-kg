3c0d4fe94d3bb831f18ab70746c5defee22da8c0
kadengrumbles73 <kaden.grumbles@gmail.com>
2026-01-29 09:45:22 -0700

    finished coolpics

---
+++ coolpics/coolpics.js
@@ -0,0 +1,37 @@
+const menuButton = document.querySelector("#menu-button");
+const navLinks = document.querySelector(".nav-links");
+
+menuButton.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    
+// Code to show modal  - Use event parameter 'e'   
+    if (e.target.tagName === 'IMG'){
+        const clickSrc = e.target.getAttribute('src');
+        const fullSrc = clickSrc.replace('-sm', '-full');
+        modalImage.src = fullSrc;
+        modal.showModal();
+    }
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
--- week3/coolpics/coolpics.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector("#menu-button");
-const navLinks = document.querySelector(".nav-links");
-
-menuButton.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
-});
