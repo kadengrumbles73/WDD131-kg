6a6405150d33a14df67c0b8ebf5b5f39c6b36434
kadengrumbles73 <kaden.grumbles@gmail.com>
2026-01-30 20:13:05 -0700

    finished cool pics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -5,4 +5,29 @@ menuButton.addEventListener("click", () => {
     navLinks.classList.toggle("show");
 });
 
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
 
+gallery.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
+        const clickedSrc = e.target.getAttribute('src');
+        
+        // Swap -sm for -full
+        const fullSrc = clickedSrc.replace('-sm', '-full');
+        
+        modalImage.src = fullSrc;
+        modalImage.alt = e.target.alt;
+        
+        modal.showModal();
+    }
+});
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
